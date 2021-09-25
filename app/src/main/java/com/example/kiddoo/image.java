e6d983d09234c3449ce1ed59f4689fed432f4920
Beuni741 <it20241414@gmail.com>
2021-09-25 13:43:31 +0530

    Animals and Multiplication

---
+++ app/src/main/java/com/example/kiddoo/image.java
@@ -0,0 +1,12 @@
+package com.example.kiddoo;
+
+public class image {
+    private  int image;
+    public image(int image){
+        this.image = image;
+    }
+
+    public int getImage(){
+        return image;
+    }
+}
